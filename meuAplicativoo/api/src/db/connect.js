3a3ec883eb22bb9a6ae7dbdaa23a2ea291cceef1
guilherme <guilherme10007@gmail.com>
2025-02-26 12:39:54 -0300

    Meu aplicativo

--- meuAplicativoo/api/src/db/connect.js
+++ meuAplicativoo/api/src/db/connect.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'10.89.240.70',
+    host:'10.89.234.160',
     user:'alunods',
     password:'senai@604',
-    database:'vio_guilherme'
+    database:'vio_guilherme11'
 })
 
 module.exports = pool;
